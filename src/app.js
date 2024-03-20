14df2dad8898ba74a2d469a98f79ff512ba349e4
Ainhoa <150278575+noaregui@users.noreply.github.com>
2024-03-20 16:34:08 +0000

    juego joker

--- src/app.js
+++ src/app.js
@@ -1,11 +1,50 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".card").classList.add(generateRandomPalos());
+  document.querySelector(".card").innerHTML += generateRandomNumber();
 };
+
+let generateRandomNumber = () => {
+  let numbers = [
+    "☠️",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
+};
+
+let generateRandomPalos = () => {
+  let palos = ["♦", "♠", "♥", "♣"];
+  let indexPalos = Math.floor(Math.random() * palos.length);
+  return palos[indexPalos];
+};
+
+let firstClick = true; //Para el 1º click: alerta
+
+document.getElementById("boton").addEventListener("click", () => {
+  if (firstClick) {
+    alert("¡Si sale ☠️ estás muerto!");
+    firstClick = false;
+  } else {
+    const cardElement = document.querySelector(".card");
+    cardElement.classList.remove("♦", "♠", "♥", "♣");
+    const randomNumber = generateRandomNumber();
+    const randomPalo = generateRandomPalos();
+    cardElement.textContent = randomNumber;
+    cardElement.setAttribute("data-card-number", randomNumber);
+    cardElement.classList.add(randomPalo);
+  }
+});
